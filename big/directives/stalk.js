22c88a1e61323b1c3e936588583d4ed1ddf629fb
Marak <marak.squires@gmail.com>
2014-10-14 15:13:53 +0200

    Make "Stalk" use enemy health instead of fixed value. Combats "Unwise Assassin"

--- big/directives/stalk.js
+++ big/directives/stalk.js
@@ -1,28 +1,25 @@
 //
-// Chases critically injured enemies that are 2 away
+// Chases critically injured enemies that less then 2 away
 //
 function stalk (gameData, helpers) {
   var direction;
   var myHero = gameData.activeHero;
 
-  if (myHero.health >= 80) {
-    // TODO: finishing blow logic / keep going if enemy is one hit away
-    var tile = findNearestWeakerEnemyTile(gameData);
+  var damagedEnemy = nearestTile(gameData, {
+    type: "Hero",
+    team: {
+      op: "NEQ",
+      val: myHero.team
+    },
+    health: {
+      op: "LTE",
+      val: 40 
+    }
+  });
 
-      var damagedEnemy = nearestTile(gameData, {
-        type: "Hero",
-        team: {
-          op: "NEQ",
-          val: myHero.team
-        },
-        health: {
-          op: "LTE",
-          val: 40 
-        }
-      });
 
-    if (damagedEnemy) {
-      //console.log(tile.distance);
+  if (damagedEnemy) {
+    if (myHero.health >= (damagedEnemy.health - 20)) {
       if(damagedEnemy.distance <= 2) {
         direction = damagedEnemy.direction;
         return direction;
